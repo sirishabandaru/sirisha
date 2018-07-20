ba4e12c7d7c50fbd9746cbfc2a380c238887ab98
sirishabandaru <41233469+sirishabandaru@users.noreply.github.com>
2018-07-20 15:44:53 +0530

    Add files via upload

---
+++ Linked_List.java
@@ -0,0 +1,18 @@
+package info;
+
+import java.util.LinkedList;
+
+public class Linked_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedList<String>ll=new LinkedList<>();
+ll.add(" ");
+ll.add("jvm");
+for (String string : ll) {
+	System.out.println(string);
+}
+
+	}
+
+}
