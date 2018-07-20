f6780e33253eb0640dc654e123dcc15aed0f124a
sirishabandaru <41233469+sirishabandaru@users.noreply.github.com>
2018-07-20 15:47:06 +0530

    Add files via upload

---
+++ list.java
@@ -0,0 +1,18 @@
+package info;
+
+import java.util.ArrayList;
+
+public class list {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<String>al=new ArrayList<>();
+al.add("siri");
+al.add(" ");
+al.add("abhi");
+for (String string : al) {
+	System.out.println(string);
+}
+	}
+
+}
