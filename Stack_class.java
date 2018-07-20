7367452bafbc450329efbadc9db31dd987677cf1
sirishabandaru <41233469+sirishabandaru@users.noreply.github.com>
2018-07-20 15:43:07 +0530

    Add files via upload

---
+++ Stack_class.java
@@ -0,0 +1,19 @@
+package siri510;
+
+import java.util.Stack;
+
+public class Stack_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Stack<Integer> s=new Stack<>();
+s.push(12);
+s.push(12);
+s.push(11);
+for (Integer integer : s) {
+	System.out.println(integer);
+	
+}
+	}
+
+}
