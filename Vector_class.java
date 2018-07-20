28b0474dd16a5fc991e425d0f60bcdabc43348c4
sirishabandaru <41233469+sirishabandaru@users.noreply.github.com>
2018-07-20 15:43:51 +0530

    Add files via upload

---
+++ Vector_class.java
@@ -0,0 +1,19 @@
+package siri510;
+import java.util.Vector;
+public class Vector_class {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<String> v=new Vector<>();
+		v.add("siri");
+		v.add("  ");
+		v.add("bala");
+		for (String string : v) {
+			System.out.println(string);		
+		}
+		
+		
+		
+
+	}
+
+}
